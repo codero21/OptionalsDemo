6ba92460600b6eaf5c441914fd1f8fc6fd82f306
Rollin Francois <rfrancois21@hotmail.com>
2020-04-27 21:42:25 -0400

    Optionals Force Unwrapping / Lines 12 - 20 Check for nil / Lines 32 - 44 Optional Binding / Lines 54 - 67 Nil Coalescing / Lines 78 - 87 Optional Chaining / 97 - 125

--- OptionalsDemo/main.swift
+++ OptionalsDemo/main.swift
@@ -6,5 +6,123 @@
 //  Copyright © 2020 Francois Technology. All rights reserved.
 //
 
-import Foundation
+
+
+
+//      Force Unwrapping
+
+//let myOptional: String?
+//
+//myOptional = "Some String"
+////let text: String = myOptional!
+//
+//print(myOptional!)
+////print(text)
+
+
+
+
+
+
+
+
+
+
+
+// Check for nil value
+
+//let myOptional: String?
+//
+////myOptional = nil
+//myOptional = "Rollin"
+//
+//if myOptional != nil {
+//    let text: String = myOptional!
+//    print(text)
+//} else {
+//    print("myOptional was nil!")
+//}
+
+
+
+
+
+
+
+
+
+//      Optional Binding
+
+//let myOptional: String?
+//
+////myOptional = nil
+//myOptional = "Rollin"
+//
+//if let safeOptional = myOptional {
+//    //let text: String = safeOptional
+//    //let text2: String = safeOptional
+//    print(safeOptional)
+//} else {
+//    print("myOptional was found to be nil.")
+//}
+
+
+
+
+
+
+
+
+
+
+//      Nil Coalescing Operator
+
+//let myOptional: String?
+//
+//myOptional = nil
+////myOptional = "5"
+//
+//let text: String = myOptional ?? "Default Text, because 'text' was nil"
+//
+//print(text)
+
+
+
+
+
+
+
+
+
+//      Optional Chaining
+
+struct MyOptional {
+    var property = 123
+
+    func method() {
+        print("I am the struct method")
+    }
+}
+
+
+// create an optional struct
+let myOptional: MyOptional?
+
+// instantiating optional struct
+myOptional = MyOptional()
+//myOptional = nil
+
+//myOptional?.method()
+//print(myOptional?.property)
+
+//          checking for nil
+if myOptional != nil {
+    print("'myOoptional' is not nil and the property value is \(myOptional!.property)")
+    myOptional?.method()
+} else {
+    print("myOptional is nil and the property value is \(String(describing: myOptional?.property))")
+    myOptional?.method()
+}
+
+
 
